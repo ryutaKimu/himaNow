bfe32869ea0f7e26086ed51af46ac2739ce3589f
ryutaKimu <rekkoilove@softbank.ne.jp>
2025-04-24 03:26:03 +0900

    add: make registerAPI

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -8,4 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
